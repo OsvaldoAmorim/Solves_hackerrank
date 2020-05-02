465252c253dceb05a2eb9d721b62d776bc784a08
Osvaldo Amorim <62940620+OsvaldoAmorim@users.noreply.github.com>
2020-05-02 01:35:52 -0300

    Create division.py
    Task
    Read two integers and print two lines. 
    The first line should contain integer division,  a//b . 
    The second line should contain float division,  a/b .
    
    You don't need to perform any rounding or formatting operations.

---
+++ division.py
@@ -0,0 +1,8 @@
+#https://www.hackerrank.com/challenges/python-division/problem
+#By: Osvaldo Amorim
+#---------------------------------
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
